615891be750f9c91324d8ddff4b6a22f551997f7
Ashish Agarwal <agarwal1975@gmail.com>
2014-03-02 16:54:21 -0500

    Travis: simplify build matrix and improve script
    Now testing only on different versions of OCaml, and with whatever is
    the latest version of Core. Build fails on OCaml 3.12.1 for an unknown
    reason. Don’t care to investigate. Script improvements are only
    cosmetic.

--- bin/travis.sh
+++ bin/travis.sh
@@ -1,34 +1,24 @@
-OPAM_DEPENDS="ocamlfind omake core.$CORE_VERSION"
-
-case "$OCAML_VERSION,$OPAM_VERSION" in
-3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
-4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+case "$OCAML_VERSION" in
+4.00.1) ppa=avsm/ocaml40+opam11 ;;
+4.01.0) ppa=avsm/ocaml41+opam11 ;;
+*) echo Unknown $OCAML_VERSION; exit 1 ;;
 esac
 
-echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo add-apt-repository -y ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra aspcud opam
+
+# configure and view settings
 export OPAMYES=1
-export OPAMVERBOSE=1
-echo OCaml version
 ocaml -version
-echo OPAM versions
 opam --version
 opam --git-version
 
+# install OCaml packages
 opam init 
 eval `opam config env`
+opam install ocamlfind omake core
 
-echo "==== Installing $OPAM_DEPENDS ===="
-opam install ${OPAM_DEPENDS}
-
-echo "==== Build ===="
+# test this package
 omake
-
-echo "==== Doc ===="
 omake doc
